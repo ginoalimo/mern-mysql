2076909ff349048187ff1f89e981e8cf070f7809
Gino A. Limo <ginoalgarcia@gmail.com>
2022-08-29 19:58:21 -0500

    modify index directory

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { PORT } from "./config.js";
+import indexRoutes from "./routes/index.routes.js";
+import taskRoutes from "./routes/tasks.routes.js";
+import cors from "cors";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(cors());
+app.use(express.json());
+app.use(indexRoutes);
+app.use(taskRoutes);
+
+app.use(express.static(join(__dirname, "../client/dist")));
+
+app.listen();
+console.log(`Server is running`);
