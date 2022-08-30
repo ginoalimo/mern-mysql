dcc9b1daee02c0852604cbf5348f75c05bcf8748
Gino A. Limo <ginoalgarcia@gmail.com>
2022-08-30 10:53:55 -0500

    db pool

--- server/db.js
+++ server/db.js
@@ -4,11 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const pool = createPool({
-    // host: 'localhost',
-    // port: 3306,
-    // user: 'root',
-    // password: 'faztpassword',
-    // database: 'taskdb',
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
