553185a6fcaa0a4dd0e4fcc1ea871cec34ecc812
Gino A. Limo <ginoalgarcia@gmail.com>
2022-08-29 19:55:18 -0500

    home

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -3,6 +3,10 @@ import { pool } from "../db.js";
 
 const router = Router();
 
+router.get("/", (req, res) => {
+  res.send("Home");
+})
+
 router.get("/ping", async (req, res) => {
   const [rows]= await pool.query("SELECT 1 + 1 as result");
   console.log(rows);
